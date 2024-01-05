5a9d0b9a50eeec947ada1d9c9aa90172a491dd99
zxxng <jeayeong123@naver.com>
2024-01-05 14:18:57 +0900

    feat: 랜덤한 색상을 생성하는 기능 추가

---
+++ DAY1/zxxng/js/ColorGenerator.js
@@ -0,0 +1,30 @@
+class ColorGenerator {
+  constructor() {
+    this.color = '#395b51';
+  }
+
+  getColor(type) {
+    if (type === 'hex') this.color = this.#generateHexColor();
+    else this.color = this.#generateSimpleColor();
+
+    return this.color;
+  }
+
+  #generateSimpleColor() {
+    const colorList = ['green', 'red', 'rgba(133,122,200)', 'royalblue', 'skyblue'];
+    const randomNumber = Math.floor(Math.random() * colorList.length);
+    return colorList[randomNumber];
+  }
+
+  #generateHexColor() {
+    let randomcolor = '#';
+    const colorList = '0123456789ABCDEF';
+    while (randomcolor.length < 7) {
+      const randomNumber = Math.floor(Math.random() * 16);
+      randomcolor += colorList[randomNumber];
+    }
+    return randomcolor;
+  }
+}
+
+export default ColorGenerator;
