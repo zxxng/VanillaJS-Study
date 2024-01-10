8616d89219db0f167949913ff896bc3de047cdf9
zxxng <jeayeong123@naver.com>
2024-01-10 12:40:25 +0900

    refactor: updateCount 메서드 Counter 클래스로 책임 분리

--- DAY2/zxxng/js/App.js
+++ DAY2/zxxng/js/App.js
@@ -3,7 +3,6 @@ import Counter from './Counter.js';
 class App {
   constructor() {
     this.controller = new Counter();
-    this.count = document.getElementById('count');
     this.decreaseBtn = document.querySelector('.decrease-btn');
     this.resetBtn = document.querySelector('.reset-btn');
     this.increaseBtn = document.querySelector('.increase-btn');
@@ -13,26 +12,13 @@ class App {
   bindEvents() {
     this.decreaseBtn.addEventListener('click', () => {
       this.controller.decreaseCount();
-      this.#updateCount();
     });
-
     this.resetBtn.addEventListener('click', () => {
       this.controller.resetCount();
-      this.#updateCount();
     });
-
     this.increaseBtn.addEventListener('click', () => {
       this.controller.increaseCount();
-      this.#updateCount();
     });
-  }
-
-  #updateCount() {
-    this.count.textContent = this.controller.getCount();
-
-    if (this.count.textContent < 0) this.count.style.color = 'red';
-    else if (this.count.textContent > 0) this.count.style.color = 'green';
-    else this.count.style.color = 'black';
   }
 }
 
--- DAY2/zxxng/js/Counter.js
+++ DAY2/zxxng/js/Counter.js
@@ -1,5 +1,6 @@
 class Counter {
   constructor() {
+    this.displayCount = document.getElementById('count');
     this.count = 0;
   }
 
@@ -9,14 +10,25 @@ class Counter {
 
   decreaseCount() {
     this.count--;
+    this.#updateCount();
   }
 
   increaseCount() {
     this.count++;
+    this.#updateCount();
   }
 
   resetCount() {
     this.count = 0;
+    this.#updateCount();
+  }
+
+  #updateCount() {
+    this.displayCount.textContent = this.count;
+
+    if (this.count < 0) this.displayCount.style.color = 'red';
+    else if (this.count > 0) this.displayCount.style.color = 'green';
+    else this.displayCount.style.color = 'black';
   }
 }
 
