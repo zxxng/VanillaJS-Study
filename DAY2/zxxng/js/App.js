ae090d4a18a8db0a625b525a3e426fc19b840977
zxxng <jeayeong123@naver.com>
2024-01-10 15:29:26 +0900

    chore: 변수명 명확하게 변경

--- DAY2/zxxng/js/App.js
+++ DAY2/zxxng/js/App.js
@@ -2,7 +2,7 @@ import Counter from './Counter.js';
 
 class App {
   constructor() {
-    this.controller = new Counter();
+    this.counter = new Counter();
     this.decreaseBtn = document.querySelector('.decrease-btn');
     this.resetBtn = document.querySelector('.reset-btn');
     this.increaseBtn = document.querySelector('.increase-btn');
@@ -11,13 +11,13 @@ class App {
 
   bindEvents() {
     this.decreaseBtn.addEventListener('click', () => {
-      this.controller.decreaseCount();
+      this.counter.decreaseCount();
     });
     this.resetBtn.addEventListener('click', () => {
-      this.controller.resetCount();
+      this.counter.resetCount();
     });
     this.increaseBtn.addEventListener('click', () => {
-      this.controller.increaseCount();
+      this.counter.increaseCount();
     });
   }
 }
