c33b3268233efd6b00ad0b591c3f6cb5f462263b
0uizi0 <0uizi0@naver.com>
2024-01-10 23:15:25 +0900

    refactor: 이벤트리스너 DOM 순회 최적화

--- DAY2/0uizi0/index.js
+++ DAY2/0uizi0/index.js
@@ -1,8 +1,8 @@
 const btns = document.querySelectorAll(".btn");
+const value = document.getElementById("value");
 
 /** 음수, 0, 양수에 따른 클래스 값 지정 */
-const changeColor = (num) => {
-  const value = document.getElementById("value");
+const changeColor = (num, value) => {
   if (num === 0) return (value.className = "zero");
   else if (num < 0) return (value.className = "negative");
   else return (value.className = "positive");
@@ -25,12 +25,11 @@ const changeNumber = (num, state) => {
 /** 버튼 클릭 시 이벤트 진행 */
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const value = document.getElementById("value");
     let count = Number(value.textContent);
     const state = btn.classList.item(1); // 'decrease' or 'zero' or 'increase'
     const changedNum = changeNumber(count, state);
     value.textContent = changedNum;
-    changeColor(changedNum);
+    changeColor(changedNum, value);
     count = changedNum;
   });
 });
