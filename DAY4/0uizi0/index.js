1f7797b51af2c0bbdf244be8c34e83878c704155
0uizi0 <0uizi0@naver.com>
2024-01-15 16:12:11 +0900

    refactor: classList.toggle을 이용한 코드 간결화

--- DAY4/0uizi0/index.js
+++ DAY4/0uizi0/index.js
@@ -2,12 +2,5 @@ const toggle = document.querySelector(".nav-toggle");
 
 toggle.addEventListener("click", () => {
   const links = document.querySelector(".links");
-  const currentClass = links.classList.item(1);
-  if (currentClass === "hide") {
-    links.classList.remove("hide");
-    links.classList.add("show");
-  } else {
-    links.classList.remove("show");
-    links.classList.add("hide");
-  }
+  links.classList.toggle("show");
 });
