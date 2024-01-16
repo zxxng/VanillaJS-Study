13e42373f8d5c553b2e176e75f2520dfadfa0fe0
zxxng <jeayeong123@naver.com>
2024-01-16 17:09:26 +0900

    fix: 데스크탑 뷰에서 메뉴 안보이는 오류 해결

--- DAY4/zxxng/app.js
+++ DAY4/zxxng/app.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.nav-toggle');
+const hide = document.querySelector('.hide');
 
 toggleBtn.addEventListener('click', () => {
-  const hide = document.querySelector('.hide');
   hide.classList.toggle('hide');
 });
